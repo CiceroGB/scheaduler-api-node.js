c030ed331809f09644d59df6f820daf06eb5c2d6
Cicero Brunelli <cicero.brunelli@gmail.com>
2020-07-23 11:15:15 -0300

    Final project

---
+++ src/app/controllers/AppointmentController.js
@@ -0,0 +1,143 @@
+import * as Yup from 'yup';
+import {
+  startOfHour, parseISO, isBefore, format, subHours,
+} from 'date-fns';
+import pt from 'date-fns/locale/pt';
+import Appointment from '../models/Appointment';
+import User from '../models/User';
+import File from '../models/File';
+import Notification from '../schemas/Notification';
+import CancellationMail from '../jobs/CancellationMail';
+
+import Queue from '../../lib/Queue';
+
+class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date', 'past', 'cancelable'],
+      limit: 20,
+      offset: (page - 1) * 20,
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+
+    });
+    return res.json(appointments);
+  }
+
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { date, provider_id } = req.body;
+
+    // Verify if provider_id is a provider
+    const isProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!isProvider) {
+      return res.status(401).json({ error: 'You only can create appointment with providers' });
+    }
+
+    // check if date is past
+    const hourStart = startOfHour(parseISO(date));
+
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not allowed' });
+    }
+
+    // check if date availability
+    const dataAvailability = await Appointment.findOne({
+      where: {
+        provider_id,
+        date: hourStart,
+        canceled_at: null,
+      },
+    });
+
+    if (dataAvailability) {
+      return res.status(400).json({ error: 'Appointment date is not available' });
+    }
+
+    // store
+    const appointment = await Appointment.create({
+      user_id: req.userId,
+      date,
+      provider_id,
+    });
+
+    // notify provider
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(
+      hourStart,
+      "'dia' dd 'de' MMMM', às' H:mm'h'",
+      { locale: pt },
+    );
+
+    await Notification.create({
+      content: `${user.name} agendou para ${formattedDate} `,
+      user: provider_id,
+    });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['name', 'email'],
+        },
+        {
+          model: User,
+          as: 'user',
+          attributes: ['id', 'name'],
+        },
+      ],
+    });
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({ error: 'You do not have permission' });
+    }
+
+    const dateSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateSub, new Date())) {
+      return res.status(401).json({ error: 'Cancellation only could be two hours before the scheduled time ' });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await Queue.add(CancellationMail.key, { appointment });
+
+    await appointment.save();
+
+    return res.json(appointment);
+  }
+}
+
+export default new AppointmentController();
